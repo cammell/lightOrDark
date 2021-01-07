8c706fb4809eef9340144841b8dcd111bb89e241
Cammell <cammell@gmail.com>
2021-01-07 23:36:15 +0100

    js changed, but still doesnt work

--- src/js/index.js
+++ src/js/index.js
@@ -9,12 +9,19 @@ import '../scss/main.scss';
 console.log('HELLO 🚀')
 
 const button=document.querySelector('.button_js');
-button.addEventListener('click', toggleLight);
-
+button.addEventListener('click', toggleLight, false);
+// button.onlick=toggleLight();
 function toggleLight() {
     
     var body=document.querySelector('.body');
-    body.classList.toggle("dark");
+    if (body.classList.contains("light")) {
+        body.classList.remove("light");
+        body.classList.add("dark");
+    } else {
+        body.classList.remove("dark");
+        body.classList.add("light");
+    }
+    document.querySelector('p').textContent("Toggle light was fired");
     console.log("function toggle was fired");
     
 };
