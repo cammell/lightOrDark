7f7b6e015865d75f3461bacaa25d7b6db6387f04
Cammell <cammell@gmail.com>
2021-01-12 21:15:36 +0100

    finally button light/dark working

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,31 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
 
-/* place your code below */
 
 console.log('HELLO 🚀')
 
-const button=document.querySelector('.button_js');
-button.addEventListener('click', toggleLight, false);
+
+
+
 // button.onlick=toggleLight();
 function toggleLight() {
     
-    var body=document.querySelector('.body');
+    const body=document.querySelector('.body');
+    const p=document.querySelector('p');
     if (body.classList.contains("light")) {
         body.classList.remove("light");
         body.classList.add("dark");
+        p.textContent="It is dark now";
     } else {
         body.classList.remove("dark");
         body.classList.add("light");
+        p.textContent="It is light now";
     }
-    document.querySelector('p').textContent("Toggle light was fired");
-    console.log("function toggle was fired");
+    
+    
     
 };
 
+
+const button=document.querySelector('.button_js');
+button.addEventListener('click', toggleLight);
